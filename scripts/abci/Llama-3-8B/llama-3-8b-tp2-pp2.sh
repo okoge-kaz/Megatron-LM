4de19c9363607ce7ca4d4265368a52b757208ecf
kazuki <kaz.tokyo.tech20@gmail.com>
2024-05-06 23:31:46 +0900

    fix: checkpoint save path

--- scripts/abci/Llama-3-8B/llama-3-8b-tp2-pp2.sh
+++ scripts/abci/Llama-3-8B/llama-3-8b-tp2-pp2.sh
@@ -69,7 +69,7 @@ GRAD_CLIP=1
 # model config
 TOKENIZER_MODEL=/groups/gag51395/hf-checkpoints/Meta-Llama-3-8B/tokenizer.json
 CHECKPOINT_DIR=/groups/gag51395/checkpoints/hf-to-megatron/Llama-3-8b/tp${TENSOR_PARALLEL_SIZE}-pp${PIPELINE_PARALLEL_SIZE}
-CHECKPOINT_SAVE_DIR=/groups/gag51395/checkpoints/Llama-3-8b/swallow-ja_8-en_1_code_1/tp${TENSOR_PARALLEL_SIZE}-pp${PIPELINE_PARALLEL_SIZE}-ct${CONTEXT_PARALLEL_SIZE}/LR${LR}-MINLR${MIN_LR}-WD${WEIGHT_DECAY}
+CHECKPOINT_SAVE_DIR=/groups/gag51395/checkpoints/Llama-3-8b/ja_9-en_1/tp${TENSOR_PARALLEL_SIZE}-pp${PIPELINE_PARALLEL_SIZE}-ct${CONTEXT_PARALLEL_SIZE}/LR${LR}-MINLR${MIN_LR}-WD${WEIGHT_DECAY}
 
 mkdir -p ${CHECKPOINT_SAVE_DIR}
 
