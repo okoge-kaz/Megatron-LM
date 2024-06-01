7cb10723fc4b583df6feafeaa6b93fc0f98b3dc3
kazuki <kaz.tokyo.tech20@gmail.com>
2024-06-01 15:44:37 +0900

    feat: convert & upload script

--- scripts/abci/upload/upload_Llama-3-70b.sh
+++ scripts/abci/upload/upload_Llama-3-70b.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-start=2500
-end=2500
+start=10000
+end=10000
 increment=2500
 
 upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3-70b/exp6/tp8-pp16-ct1/LR1.0e-5-MINLR1.0E-6-WD0.1
