d920cf49bd175009f240d7d1778a9e8bdff15a18
kazuki <kaz.tokyo.tech20@gmail.com>
2024-06-01 15:44:09 +0900

    feat: update change tp, pp convert script

--- scripts/abci/change_tp_pp.sh
+++ scripts/abci/change_tp_pp.sh
@@ -9,37 +9,38 @@ set -e
 
 # module load
 source /etc/profile.d/modules.sh
-module load cuda/11.8/11.8.0
-module load cudnn/8.9/8.9.2
-module load nccl/2.16/2.16.2-1
+module use /groups/gag51395/modules/modulefiles
+
+module load cuda/12.1/12.1.1
+module load cudnn/cuda-12.1/9.0.0
+module load nccl/2.17/2.17.1-1
 module load hpcx/2.12
+module load gcc/11.4.0
 
 # python virtualenv
-cd /bb/llm/gaf51275/llama/Megatron-LM
 source .env/bin/activate
 
 # distributed settings
-TARGET_TENSOR_PARALLEL_SIZE=1   # fixed
-TARGET_PIPELINE_PARALLEL_SIZE=1 # fixed
+TARGET_TENSOR_PARALLEL_SIZE=2
+TARGET_PIPELINE_PARALLEL_SIZE=4
 
-BASE_TENSOR_PARALLEL_SIZE=8
-BASE_PIPELINE_PARALLEL_SIZE=8
+BASE_TENSOR_PARALLEL_SIZE=2
+BASE_PIPELINE_PARALLEL_SIZE=2
 
 # model config
-BASE_CHECKPOINT_DIR=/bb/llm/gaf51275/llama/llama-megatron-convert-checkpoint-hf/Llama-2-70b/tp${BASE_TENSOR_PARALLEL_SIZE}-pp${BASE_PIPELINE_PARALLEL_SIZE}
-TARGET_CHECKPOINT_DIR=/bb/llm/gaf51275/llama/llama-megatron-convert-checkpoint-hf/Llama-2-70b/tp${TARGET_TENSOR_PARALLEL_SIZE}-pp${TARGET_PIPELINE_PARALLEL_SIZE}
+BASE_CHECKPOINT_DIR=/bb/llm/gaf51275/2024/checkpoints/Llama-3-8b/exp6/tp${BASE_TENSOR_PARALLEL_SIZE}-pp${BASE_PIPELINE_PARALLEL_SIZE}-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1-WARMUP1000
+TARGET_CHECKPOINT_DIR=/bb/llm/gaf51275/2024/checkpoints/Llama-3-8b/exp6/tp${TARGET_TENSOR_PARALLEL_SIZE}-pp${TARGET_PIPELINE_PARALLEL_SIZE}-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1-WARMUP1000
 
 mkdir -p ${TARGET_CHECKPOINT_DIR}
 
-# tokenizer config
-TOKENIZER_MODEL=/bb/llm/gaf51275/llama/huggingface-checkpoint/Llama-2-70b-hf/tokenizer.model
+export CUDA_DEVICE_MAX_CONNECTIONS=1
 
 # convert
-python tools/checkpoint/util.py \
+python tools/checkpoint/convert.py \
   --model-type GPT \
-  --loader megatron \
-  --saver megatron \
-  --megatron-path /bb/llm/gaf51275/llama/Megatron-LM \
+  --loader mcore \
+  --saver mcore \
+  --megatron-path /bb/llm/gaf51275/2024/Megatron-LM \
   --target-tensor-parallel-size ${TARGET_TENSOR_PARALLEL_SIZE} \
   --target-pipeline-parallel-size ${TARGET_PIPELINE_PARALLEL_SIZE} \
   --load-dir ${BASE_CHECKPOINT_DIR} \
