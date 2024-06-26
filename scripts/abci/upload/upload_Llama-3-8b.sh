dc9628534909a3201d3a5f393f55663f71977742
kazuki <kaz.tokyo.tech20@gmail.com>
2024-06-26 12:04:23 +0900

    chore: checkpoint convert & upload

--- scripts/abci/upload/upload_Llama-3-8b.sh
+++ scripts/abci/upload/upload_Llama-3-8b.sh
@@ -3,15 +3,15 @@
 set -e
 
 start=2500
-end=2500
+end=10000
 increment=2500
 
-upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3-8b-hf/exp9/tp2-pp4-ct1/LR2.5e-5-MINLR2.5E-6-WD0.1
+upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3-8b-hf/exp10/tp2-pp4-ct1/LR2.5e-5-MINLR2.5E-6-WD0.1
 
 for ((i = start; i <= end; i += increment)); do
   upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
 
   python scripts/abci/upload/upload.py \
     --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Llama-3-8b-exp9-LR2.5e-5-MINLR2.5E-6-WD0.1-iter$(printf "%07d" $i)
+    --repo-name tokyotech-llm/Llama-3-8b-exp10-LR2.5e-5-MINLR2.5E-6-WD0.1-iter$(printf "%07d" $i)
 done
