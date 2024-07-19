dbe5d12f87358a43159a0b58ffa56fa489164daf
kazuki <kaz.tokyo.tech20@gmail.com>
2024-07-19 22:02:15 +0900

    feat: meta tag exp5

--- scripts/abci/upload/upload_Llama-3-8b.sh
+++ scripts/abci/upload/upload_Llama-3-8b.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-start=7500
-end=7500
+start=12500
+end=12500
 increment=2500
 
 upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3-8b-meta-tag/exp5/tp2-pp4-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1
