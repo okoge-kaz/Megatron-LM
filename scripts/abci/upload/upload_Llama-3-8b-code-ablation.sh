2ace4072c4fea9ffa0c9bbec781f5d846fb7f529
kazuki <kaz.tokyo.tech20@gmail.com>
2024-07-23 10:17:35 +0900

    feat: code ablation checkpoint upload

--- scripts/abci/upload/upload_Llama-3-8b-code-ablation.sh
+++ scripts/abci/upload/upload_Llama-3-8b-code-ablation.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-start=5000
-end=5000
+start=7000
+end=7000
 increment=2500
 
 upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3-8b-code-ablation/exp1/tp4-pp4-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1
