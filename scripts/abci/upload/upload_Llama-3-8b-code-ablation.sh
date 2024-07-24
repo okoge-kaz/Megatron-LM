46adf2b2605b8d584197f3add6973deccce80791
kazuki <kaz.tokyo.tech20@gmail.com>
2024-07-24 21:45:17 +0900

    feat: code ablation exp2

--- scripts/abci/upload/upload_Llama-3-8b-code-ablation.sh
+++ scripts/abci/upload/upload_Llama-3-8b-code-ablation.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-start=7000
-end=7000
+start=2500
+end=2500
 increment=2500
 
-upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3-8b-code-ablation/exp1/tp4-pp4-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1
+upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3-8b-code-ablation/exp2/tp4-pp4-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1
 
 for ((i = start; i <= end; i += increment)); do
   upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
 
   python scripts/abci/upload/upload.py \
     --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Llama-3-8b-code-ablation-exp1-LR2.5e-5-MINLR2.5E-6-WD0.1-iter$(printf "%07d" $i)
+    --repo-name tokyotech-llm/Llama-3-8b-code-ablation-exp2-LR2.5e-5-MINLR2.5E-6-WD0.1-iter$(printf "%07d" $i)
 done
