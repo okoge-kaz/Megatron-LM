913fb4e752f7031797875aa01c847ad4a17c2315
kazuki <kaz.tokyo.tech20@gmail.com>
2024-07-28 11:53:04 +0900

    feat: exp10 checkpoint

--- scripts/abci/upload/upload_Llama-3-8b.sh
+++ scripts/abci/upload/upload_Llama-3-8b.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-start=7500
-end=10000
+start=12500
+end=12500
 increment=2500
 
-EXPERIMENT_NAME=fineweb-edu
+EXPERIMENT_NAME=exp10
 
 upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3-8b/${EXPERIMENT_NAME}/tp2-pp4-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1
 
