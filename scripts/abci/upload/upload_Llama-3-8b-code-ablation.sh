76bec8d6d861001a75d2f67e338b954026b214e3
kazuki <kaz.tokyo.tech20@gmail.com>
2024-07-29 11:48:10 +0900

    feat: code ablation checkpoint convert

--- scripts/abci/upload/upload_Llama-3-8b-code-ablation.sh
+++ scripts/abci/upload/upload_Llama-3-8b-code-ablation.sh
@@ -2,31 +2,13 @@
 
 set -e
 
-start=2500
-end=5000
-increment=2500
-
-upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3-8b-code-ablation/exp3/tp4-pp4-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1
-
-for ((i = start; i <= end; i += increment)); do
-  upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
-
-  python scripts/abci/upload/upload.py \
-    --ckpt-path $upload_dir \
-    --repo-name tokyotech-llm/Llama-3-8b-code-ablation-exp3-LR2.5e-5-MINLR2.5E-6-WD0.1-iter$(printf "%07d" $i)
-done
-
-#!/bin/bash
-
-set -e
-
-start=2500
-end=5000
+start=7500
+end=10000
 increment=2500
 
 EXPERIMENT_NAME=exp3
 
-upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3-8b-code-ablation/${EXPERIMENT_NAME}/tp2-pp4-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1
+upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3-8b-code-ablation/${EXPERIMENT_NAME}/tp4-pp4-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1
 
 upload_checkpoint() {
   local upload_dir=$1
