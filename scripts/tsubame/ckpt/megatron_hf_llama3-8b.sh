a15acf2cb23d0c284fb340614ed6019911f31ebd
kazuki <kaz.tokyo.tech20@gmail.com>
2024-08-22 21:22:21 +0900

    feat: checkpoint convert script

--- scripts/tsubame/ckpt/megatron_hf_llama3-8b.sh
+++ scripts/tsubame/ckpt/megatron_hf_llama3-8b.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #$ -cwd
 #$ -l node_f=1
-#$ -l h_rt=3:00:00
-#$ -o outputs/convert/megatron_hf/$JOB_ID
-#$ -e outputs/convert/megatron_hf/$JOB_ID
+#$ -l h_rt=10:00:00
+#$ -o outputs/convert/megatron_hf/$JOB_ID.log
+#$ -e outputs/convert/megatron_hf/$JOB_ID.log
 #$ -p -5
 
 # Load modules
@@ -22,28 +22,33 @@ source .env/bin/activate
 TENSOR_PARALLEL_SIZE=2
 PIPELINE_PARALLEL_SIZE=2
 
-ITERATION=7500
-FORMATTED_ITERATION=$(printf "%07d" $ITERATION)
+START_ITERATION=500
+END_ITERATION=12500
+INCREMENT=500
 
-# model config
-MEGATRON_CHECKPOINT_DIR=/gs/bs/tga-NII-LLM/checkpoints/Llama-3-8b-block-expanison-36/exp4/tp2-pp2-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1-WARMUP1000
-HF_CHECKPOINT_DIR=/gs/bs/tga-NII-LLM/checkpoints/megatron-to-hf/Llama-3-8b-block-expanison-36/exp4/tp4-pp2-ct1/LR2.5E-5-MINLR2.5E-6-WD0.1-WARMUP1000/iter_${FORMATTED_ITERATION}
+for ITERATION in $(seq $START_ITERATION $INCREMENT $END_ITERATION); do
+  FORMATTED_ITERATION=$(printf "%07d" $ITERATION)
 
-mkdir -p ${HF_CHECKPOINT_DIR}
+  # model config
+  MEGATRON_CHECKPOINT_DIR=/gs/bs/tga-NII-LLM/checkpoints/Llama-3-8b/exp6-docs/tp2-pp2-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1-WARMUP1000
+  HF_CHECKPOINT_DIR=/gs/bs/tga-NII-LLM/checkpoints/megatron-to-hf/Llama-3-8b/exp6-intra-document/tp2-pp2-ct1/LR2.5E-5-MINLR2.5E-6-WD0.1/iter_${FORMATTED_ITERATION}
 
-echo $ITERATION > "${MEGATRON_CHECKPOINT_DIR}/latest_checkpointed_iteration.txt"
+  mkdir -p ${HF_CHECKPOINT_DIR}
 
-# tokenizer config
-TOKENIZER_MODEL_DIR=/gs/bs/tga-bayes-crest/fujii/hf-checkpoints/Meta-Llama-3-8B
+  echo $ITERATION > "${MEGATRON_CHECKPOINT_DIR}/latest_checkpointed_iteration.txt"
 
-# convert
-python tools/checkpoint/convert.py \
-  --model-type GPT \
-  --loader mcore \
-  --saver llama3_hf \
-  --load-dir ${MEGATRON_CHECKPOINT_DIR} \
-  --save-dir ${HF_CHECKPOINT_DIR} \
-  --hf-tokenizer-path ${TOKENIZER_MODEL_DIR} \
-  --save-dtype bfloat16 \
-  --loader-transformer-impl transformer_engine \
-  --megatron-path /gs/bs/tga-bayes-crest/fujii/Megatron-LM
+  # tokenizer config
+  TOKENIZER_MODEL_DIR=/gs/bs/tga-bayes-crest/fujii/hf-checkpoints/Meta-Llama-3-8B
+
+  # convert
+  python tools/checkpoint/convert.py \
+    --model-type GPT \
+    --loader mcore \
+    --saver llama3_hf \
+    --load-dir ${MEGATRON_CHECKPOINT_DIR} \
+    --save-dir ${HF_CHECKPOINT_DIR} \
+    --hf-tokenizer-path ${TOKENIZER_MODEL_DIR} \
+    --save-dtype bfloat16 \
+    --loader-transformer-impl transformer_engine \
+    --megatron-path /gs/bs/tga-bayes-crest/fujii/Megatron-LM
+done
