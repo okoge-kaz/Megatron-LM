a24ece246722d8f41290bca7b58e09a6abbff944
kazuki <kaz.tokyo.tech20@gmail.com>
2024-09-04 13:47:28 +0900

    chore: checkpoint convert & upload script

--- scripts/abci/upload/upload_Llama-3.1-8b.sh
+++ scripts/abci/upload/upload_Llama-3.1-8b.sh
@@ -6,7 +6,7 @@ start=2500
 end=2500
 increment=2500
 
-EXP=exp2
+EXP=exp4
 
 upload_base_dir=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3.1-8b-ablation/${EXP}/tp4-pp2-ct1-LR2.5E-5-MINLR2.5E-6-WD0.1
 
