49fc11170c34013b2529b96c7821f73751222d38
kazuki <kaz.tokyo.tech20@gmail.com>
2024-10-19 11:37:07 +0900

    feat: code ablation checkpoint convert script

---
+++ scripts/abci/convert_hf_megatron/megatron_hf_llama3.1-8b-code-ablation.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#$ -l rt_AF=1
+#$ -l h_rt=0:5:00:00
+#$ -j y
+#$ -o outputs/megatron-to-hf/
+#$ -cwd
+
+# Load modules
+source /etc/profile.d/modules.sh
+module use /bb/llm/gaf51275/modules/modulefiles
+
+module load cuda/12.1/12.1.1
+module load cudnn/cuda-12.1/9.0.0
+module load nccl/2.20.5
+module load hpcx/2.12
+module load gcc/11.4.0
+
+# switch virtual env
+source .env/bin/activate
+
+# distributed settings
+TENSOR_PARALLEL_SIZE=4
+PIPELINE_PARALLEL_SIZE=1
+
+# iteration settings
+START_ITERATION=2500
+END_ITERATION=12500
+STEP=2500
+
+EXP=exp4
+
+# model config
+MEGATRON_CHECKPOINT_DIR=/bb/llm/gaf51275/2024/checkpoints/Llama-3.1-8b-code/exp4/tp4-pp1-ct2/LR2.5E-5-MINLR2.5E-6-WD0.1
+
+# tokenizer config
+TOKENIZER_MODEL_DIR=/bb/llm/gaf51275/hf-checkpoints/Meta-Llama-3.1-8B
+
+# hf checkpoint dir base
+HF_CHECKPOINT_DIR_BASE=/bb/llm/gaf51275/2024/checkpoints/megatron-to-hf/Llama-3.1-8b-code-ablation/${EXP}/tp${TENSOR_PARALLEL_SIZE}-pp${PIPELINE_PARALLEL_SIZE}
+
+mkdir -p ${HF_CHECKPOINT_DIR_BASE}
+
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+# iterate through specified iterations
+for ITERATION in $(seq $START_ITERATION $STEP $END_ITERATION); do
+  FORMATTED_ITERATION=$(printf "%07d" $ITERATION)
+  HF_CHECKPOINT_DIR=${HF_CHECKPOINT_DIR_BASE}/iter_${FORMATTED_ITERATION}
+
+  mkdir -p ${HF_CHECKPOINT_DIR}
+
+  CURRENT_ITERATION=$(cat "${MEGATRON_CHECKPOINT_DIR}/latest_checkpointed_iteration.txt")
+
+  echo $ITERATION > "${MEGATRON_CHECKPOINT_DIR}/latest_checkpointed_iteration.txt"
+
+  echo "saving to ${HF_CHECKPOINT_DIR}"
+
+  # convert
+  python tools/checkpoint/convert.py \
+    --model-type GPT \
+    --loader mcore \
+    --saver llama3_hf \
+    --load-dir ${MEGATRON_CHECKPOINT_DIR} \
+    --save-dir ${HF_CHECKPOINT_DIR} \
+    --hf-tokenizer-path ${TOKENIZER_MODEL_DIR} \
+    --save-dtype bfloat16 \
+    --loader-transformer-impl transformer_engine \
+    --true-vocab-size 128256 \
+    --megatron-path /bb/llm/gaf51275/2024/Megatron-LM-v0.8 \
+    --llama-3-1
+
+  # reset checkpoint iteration
+  echo $CURRENT_ITERATION > "${MEGATRON_CHECKPOINT_DIR}/latest_checkpointed_iteration.txt"
+done
+
+echo "checkpoint conversion completed"
