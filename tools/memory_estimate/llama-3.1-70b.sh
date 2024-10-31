d19bb5f3e8c98435857417605a4b9d49baed2be4
kazuki <kaz.tokyo.tech20@gmail.com>
2024-10-31 13:38:14 +0900

    feat: memory consumption estimator

--- tools/memory_estimate/llama-3.1-70b.sh
+++ tools/memory_estimate/llama-3.1-70b.sh
@@ -2,21 +2,21 @@
 
 source .env/bin/activate
 
-WORLD_SIZE=8
-TENSOR_PARALLEL_SIZE=1
-CONTEXT_PARALLEL_SIZE=4
-PIPELINE_PARALLEL_SIZE=2
-MICRO_BATCH_SIZE=1
+WORLD_SIZE=128
+TENSOR_PARALLEL_SIZE=8
+CONTEXT_PARALLEL_SIZE=2
+PIPELINE_PARALLEL_SIZE=4
+MICRO_BATCH_SIZE=2
 DATA_PARALLEL_SIZE=$(($WORLD_SIZE / ($TENSOR_PARALLEL_SIZE * $CONTEXT_PARALLEL_SIZE * $PIPELINE_PARALLEL_SIZE)))
 
 echo "DATA_PARALLEL_SIZE: $DATA_PARALLEL_SIZE"
 
 python tools/memory_estimate/memory_consumption_estimator.py \
-  --hidden-size 4096 \
-  --ffn-hidden-size 14336 \
-  --seq-length 32768 \
-  --num-layers 32 \
-  --num-attention-heads 32 \
+  --hidden-size 8192 \
+  --ffn-hidden-size 28672 \
+  --seq-length 8192 \
+  --num-layers 80 \
+  --num-attention-heads 64 \
   --group-query-attention \
   --swiglu \
   --num-key-value-heads 8 \
